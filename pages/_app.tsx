202f30546e47ded8b716cb782a1186a3f8189b8e
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-07-28 01:18:38 +0600

    -  project setup done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter, Libre_Bodoni, Plus_Jakarta_Sans } from "next/font/google";
+
+export const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
+
+export const libre_bodoni = Libre_Bodoni({
+	subsets: ["latin"],
+	variable: "--font-libre-bodoni",
+});
+export const plus_jakarta_sans = Plus_Jakarta_Sans({
+	subsets: ["latin"],
+	variable: "--font-plus-jakarta-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main
+			className={`${inter.variable} ${plus_jakarta_sans.variable} ${libre_bodoni.variable} bg-[#111114] min-h-screen`}
+		>
+			<Component {...pageProps} />
+		</main>
+	);
 }
+
